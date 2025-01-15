d8e9553a030b75005a036ee909b2771d32224eb6
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2025-01-15 17:15:17 +0800

    import library in api middleware

--- app/Laravel/Middlewares/Api/JWTAuth.php
+++ app/Laravel/Middlewares/Api/JWTAuth.php
@@ -2,6 +2,12 @@
 
 namespace App\Laravel\Middlewares\Api;
 
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenBlacklistedException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Http\Middleware\BaseMiddleware;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth as Jwt;
+
 class JWTAuth extends BaseMiddleware{
 
 }
